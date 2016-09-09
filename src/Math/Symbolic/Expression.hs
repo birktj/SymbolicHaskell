66b2380206828c36d2c66aea5e959bdfd2801f4b
Birk Tjelmeland <birktjelmeland@yahoo.no>
2016-09-09 14:00:40 +0200

    New branch with simpler AST

--- src/Math/Symbolic/Expression.hs
+++ src/Math/Symbolic/Expression.hs
@@ -1,83 +1,25 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Math.Symbolic.Expression (MExpression (..), calculate, ) where
+module Math.Symbolic.Expression (MExpression (..)) where
 
 import Data.Word
 import Data.Maybe
 import Data.List
 import Data.Ratio
 import Data.String
+import Data.Monoid
+import Control.Arrow
+import qualified Data.Text as T
+import Data.Text (Text)
 
 
 data MExpression a = MNum a
-                   | MConst Word32
-                   | MVar String
-                   | MMultiOp Word32 [MExpression a]
-                   | MBiOp Word32 (MExpression a) (MExpression a)
-                   | MUnOp Word32 (MExpression a)
-                   | MErr Word32 String
+                   | MOp Text [MExpression a]
+                   | MTerm a (MExpression a) a
+                   | MConst Text
+                   | MVar Text
+                --  | MErr Word32 Text
                    deriving (Eq)
 
--- Opperator table --
----------------------
--- +      : 0x01   --
--- -      : 0x02   --
--- *      : 0x03   --
--- /      : 0x04   --
--- abs    : 0x05   --
--- pow    : 0x06   --
--- sqrt   : 0x07   --
--- log    : 0x08   --
--- exp    : 0x09   --
--- sin    : 0x0a   --
--- cos    : 0x0b   --
--- tan    : 0x0c   --
--- asin   : 0x0d   --
--- acos   : 0x0e   --
--- atan   : 0x0f   --
--- sinh   : 0x10   --
--- cosh   : 0x11   --
--- tanh   : 0x12   --
--- asinh  : 0x13   --
--- acosh  : 0x14   --
--- atanh  : 0x15   --
--- sign   : 0x31   --
----------------------
-
-
--- Constant table --
---------------------
--- pi    : 0x21   --
--- e     : 0x22   --
---------------------
-
-getOp :: Word32 -> String
-getOp 0x01 = "+"
-getOp 0x02 = "-"
-getOp 0x03 = "*"
-getOp 0x04 = "/"
-getOp 0x05 = "abs"
-getOp 0x06 = "^"
-getOp 0x07 = "sqrt"
-getOp 0x08 = "log"
-getOp 0x09 = "exp"
-getOp 0x0a = "sin"
-getOp 0x0b = "cos"
-getOp 0x0c = "tan"
-getOp 0x0d = "asin"
-getOp 0x0e = "acos"
-getOp 0x0f = "atan"
-getOp 0x10 = "sinh"
-getOp 0x11 = "cosh"
-getOp 0x12 = "tanh"
-getOp 0x13 = "asinh"
-getOp 0x14 = "acosh"
-getOp 0x15 = "atanh"
-getOp x    = "op" ++ (show x)
-
-opCompare :: Word32 -> Word32 -> Ordering
-opCompare x y | x == y = EQ
-              | x < y = GT
-              | otherwise = LT
 
 
 parens :: MExpression a -> Bool
@@ -85,38 +27,71 @@ parens :: MExpression a -> Bool
 parens (MNum _) = False
 parens (MConst _) = False
 parens (MVar _) = False
-parens (MBiOp 0x01 _ _) = True
 parens _ = True
-
 
 instance Show a => Show (MExpression a) where
     show (MNum x) = show x
-    show (MConst 0x21) = "pi"
-    show (MConst 0x22) = "e"
-    show (MConst x) = 'C':(show x)
-    show (MVar str) = str
-    show (MMultiOp op xs) = (getOp op) ++ " " ++ (show $ map (show) xs)
-    show (MBiOp op x y) | parens x && parens y = "(" ++ show x ++ ") " ++ getOp op ++ " (" ++ show y ++ ")"
-                        | parens x = "(" ++ show x ++ ") " ++ getOp op ++ " " ++ show y
-                        | parens y = show x ++ " " ++ getOp op ++ " (" ++ show y ++ ")"
-                        | otherwise = show x ++ " " ++ getOp op ++ " " ++ show y
-    show (MUnOp op x) | parens x  = getOp op ++ " (" ++ show x ++ ")"
-                      | otherwise = getOp op ++ " " ++ show x
+    show (MConst "pi") = "pi"
+    show (MConst "e") = "e"
+    show (MConst x) = 'C' : T.unpack x
+    show (MVar str) = T.unpack str
+    show (MTerm n x p) = show n <> "*" <> show x <> "^" <> show p
+    show (MOp op xs) = T.unpack op <> show xs
+    --show (MMultiOp op xs) = (getOp op) ++ " " ++ (show $ map (show) xs)
+    --show (MBiOp op x y) | parens x && parens y = "(" ++ show x ++ ") " ++ getOp op ++ " (" ++ show y ++ ")"
+    --                    | parens x = "(" ++ show x ++ ") " ++ getOp op ++ " " ++ show y
+    --                    | parens y = show x ++ " " ++ getOp op ++ " (" ++ show y ++ ")"
+    --                    | otherwise = show x ++ " " ++ getOp op ++ " " ++ show y
+    --show (MUnOp op x) | parens x  = getOp op ++ " (" ++ show x ++ ")"
+    --                  | otherwise = getOp op ++ " " ++ show x
 
-    show (MErr code errval) = "Error " ++ (show code) ++ ", with message: " ++ errval
+--    show (MErr code errval) = "Error " <> show code <> ", with message: " <> T.unpack errval
 
+
+isInfix :: Text -> Bool
+isInfix x = x `elem` ["+", "*", "/", "^"]
+
+percedence :: Text -> Int
+percedence "+" = 1
+percedence "*" = 2
+percedence "/" = 2
+percedence "^" = 3
+percedence _   = -1
+
+ifEq :: Ordering -> Ordering -> Ordering
+ifEq EQ a = a
+ifEq a _  = a
+
+getOrd :: [Ordering] -> Ordering
+getOrd (EQ:xs) = getOrd xs
+getOrd [] = EQ
+getOrd (a:_) = a
 
 instance Ord a => Ord (MExpression a) where
-    compare (MBiOp op1 _ _) (MBiOp op2 _ _)   = opCompare op1 op2
-    compare (MBiOp op1 _ _) (MMultiOp op2 _)  = opCompare op1 op2
-    compare (MMultiOp op1 _) (MMultiOp op2 _) = opCompare op1 op2
-    compare (MMultiOp op1 _) (MBiOp op2 _ _)  = opCompare op1 op2
-    compare (MMultiOp _ _) _                  = GT
-    compare (MBiOp _ _ _) _                   = GT
-    compare (MUnOp _ x) y                     = compare x y
-    compare x (MUnOp _ y)                     = compare x y
-    compare _ (MMultiOp _ _)                  = LT
-    compare _ (MBiOp _ _ _)                   = LT
+    --compare (MBiOp op1 x1 y1) (MBiOp op2 x2 y2) | op1 == op2 = compare y1 y2
+    --                                            | otherwise  = opCompare op1 op2
+    --compare (MBiOp op1 _ _) (MMultiOp op2 _)  = opCompare op1 op2
+    --compare (MMultiOp op1 _) (MMultiOp op2 _) = opCompare op1 op2
+    --compare (MMultiOp op1 _) (MBiOp op2 _ _)  = opCompare op1 op2
+    --compare (MMultiOp _ _) _                  = GT
+    --compare (MBiOp _ _ _) _                   = GT
+    --compare (MUnOp _ x) y                     = compare x y
+    --compare x (MUnOp _ y)                     = compare x y
+    --compare _ (MMultiOp _ _)                  = LT
+    --compare _ (MBiOp _ _ _)                   = LT
+    compare (MOp "-" [x]) y = compare x y
+    compare x (MOp "-" [y]) = compare x y
+
+    compare (MOp x xs) (MOp y ys) | isInfix x && isInfix y = compare (percedence y) (percedence x)
+                                                             `ifEq` getOrd (zipWith compare xs ys)
+
+    compare MOp{} _ = GT
+    compare _ MOp{} = LT
+
+    compare (MTerm n1 x1 p1) (MTerm n2 x2 p2) = compare p1 p2 `ifEq` (compare x1 x2 `ifEq` compare n1 n2)
+    compare MTerm{} _ = GT
+    compare _ MTerm{} = LT
+
     compare (MConst x) (MConst y) = EQ
     compare (MConst x) _ = GT
     compare _ (MConst x) = LT
@@ -126,58 +101,75 @@ instance Ord a => Ord (MExpression a) where
     compare (MVar x) _ = GT
     compare _ (MVar x) = LT
     compare (MNum x) (MNum y) = compare x y
-    compare _ _ = EQ
 
-calc op x y = calculate $ MBiOp op x y
+
+flatten :: MExpression a -> MExpression a
+flatten (MOp "+" xs) = MOp "+" . concat $ flatten' <$> xs
+    where
+        flatten' (MOp "+" xs) = xs
+        flatten' y = [y]
+
+flatten (MOp "*" xs) = MOp "*" . concat $ flatten' <$> xs
+    where
+        flatten' (MOp "*" xs) = xs
+        flatten' y = [y]
+
+
+getTerm :: (Num a) => MExpression a -> MExpression a
+getTerm (MOp "*" [MNum n, MTerm n' x p]) = MTerm (n * n') x p
+getTerm (MOp "*" [MNum n, MOp "^" [x, MNum p]]) = MTerm n x p
+getTerm (MOp "*" [MNum n, x]) = MTerm n x 1
+getTerm (MOp "^" [x, MNum p]) = MTerm 1 x p
+getTerm x = x
+
+traverseM :: (MExpression a -> MExpression a) -> MExpression a -> MExpression a
+traverseM f (MOp x xs) = f . MOp x $ traverseM f <$> xs
+traverseM f e = f e
+
+eqTerm :: (Eq a) => MExpression a -> MExpression a -> Bool
+eqTerm (MTerm _ x p1) (MTerm _ y p2) = x == y && p1 == p2
+eqTerm MNum{} MNum{} = True
+eqTerm (MConst x) (MConst y) = x == y
+eqTerm (MVar x) (MVar y) = x == y
+eqTerm (MOp x xs) (MOp y ys) = x == y && and (zipWith eqTerm xs ys)
+eqTerm _ _ = False
+
 
 instance (Ord a, Fractional a) => Num (MExpression a) where
-    x + y = calc 0x01 x y
-    x - y = calc 0x02 x y
-    x * y = calc 0x03 x y
-    abs x = MUnOp 0x05 x
-    negate x = MUnOp 0x02 x
-    signum x = MUnOp 0x31 x
+    x + y = traverseM getTerm . flatten $ MOp "+" [x, y]
+    x - y = traverseM getTerm . flatten $ MOp "+" [x, negate y]
+    x * y = traverseM getTerm . flatten $ MOp "*" [x, y]
+    --x + y = calc 0x01 x y
+    --x - y = calc 0x02 x y
+    --x * y = calc 0x03 x y
+    abs x    = MOp "abs" [x]
+    negate x = MOp "-" [x]
+    signum x = MOp "signum" [x]
+    --abs x = MUnOp 0x05 x
+    --negate x = MUnOp 0x02 x
+    --signum x = MUnOp 0x31 x
     fromInteger x = MNum (fromInteger x)
 
 instance (Ord a, Fractional a) => Fractional (MExpression a) where
-    x / y = calc 0x04 x y
+    x / y = MOp "/" [x, y]
     fromRational x = MNum (fromRational x)
 
 instance (Ord a, Fractional a) => Floating (MExpression a) where
-    pi = MConst 0x21
-    exp x = MBiOp 0x06 (MConst 0x22) x
-    log x = MUnOp 0x08 x
-    sqrt x = MBiOp 0x07 2 x
-    x ** y = MBiOp 0x06 x y
-    logBase x y = MBiOp 0x08 x y
-    sin x = MUnOp 0x0a x
-    cos x = MUnOp 0x0b x
-    tan x = MUnOp 0x0c x
-    asin x = MUnOp 0x0d x
-    acos x = MUnOp 0x0e x
-    atan x = MUnOp 0x0f x
-    sinh x = MUnOp 0x10 x
-    cosh x = MUnOp 0x11 x
-    tanh x = MUnOp 0x12 x
-    asinh x = MUnOp 0x13 x
-    acosh x = MUnOp 0x14 x
-    atanh x = MUnOp 0x15 x
-
-canCalc (MNum x) = True
-canCalc _ = False
-
---rearrange :: MExpression a -> MExpression a
---rearrange
-
-calculate :: (Ord a, Fractional a) => MExpression a -> MExpression a
-calculate (MBiOp 0x01 (MNum x) (MNum y)) = MNum $ x + y
-calculate (MBiOp 0x02 (MNum x) (MNum y)) = MNum $ x - y
-calculate (MBiOp 0x03 (MNum x) (MNum y)) = MNum $ x * y
-calculate (MBiOp 0x04 (MNum x) (MNum y)) = MNum $ x / y
-calculate expr@(MBiOp _ (MNum _) (MNum _)) = expr
-
-calculate (MUnOp 0x02 (MNum x)) = MNum (-x)
-calculate (MBiOp op x y) | canCalc (calculate x) && canCalc (calculate y) = calculate $ MBiOp op (calculate x) (calculate y)
-                         | otherwise = MBiOp op (calculate x) (calculate y)
---calculate (MUnOp op x)   | canCalc (calculate x) = calculate $ MUnOp op (calculate x)
-calculate x = x
+    pi = MConst "pi"
+    exp x = MOp "^" [MConst "e", x]
+    log x = MOp "log" [x]
+    sqrt x = MOp "sqrt" [x]
+    x ** y = MOp "^" [x, y]
+    logBase x y = MOp "logBase" [x, y]
+    sin x = MOp "sin" [x]
+    cos x = MOp "cos" [x]
+    tan x = MOp "tan" [x]
+    asin x = MOp "asin" [x]
+    acos x = MOp "acos" [x]
+    atan x = MOp "atan" [x]
+    sinh x = MOp "sinh" [x]
+    cosh x = MOp "cosh" [x]
+    tanh x = MOp "tanh" [x]
+    asinh x = MOp "asinh" [x]
+    acosh x = MOp "acosh" [x]
+    atanh x = MOp "atanh" [x]
