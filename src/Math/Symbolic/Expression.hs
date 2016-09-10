8b2f851da539aa403bd3f7ec109e70ad892e1e3d
Birk Tjelmeland <birktjelmeland@yahoo.no>
2016-09-10 16:10:59 +0200

    Better simplification rules

--- src/Math/Symbolic/Expression.hs
+++ src/Math/Symbolic/Expression.hs
@@ -12,189 +12,75 @@ import qualified Data.Text as T
 import Data.Text (Text)
 
 
-data MExpression a = MNum a
-                   | MOp Text [MExpression a]
-                   | MTerm a (MExpression a) a
-                   | MConst Text
-                   | MVar Text
-                --  | MErr Word32 Text
-                   deriving (Eq)
+data Math a = Numeric a
+            | Op Text [Math a]
+            | Sym Text
+            deriving (Eq)
 
 
+instance Show a => Show (Math a) where
+    show (Numeric x) = show x
+    show (Sym str) = T.unpack str
+    show (Op op xs) = T.unpack op <> show xs
 
-parens :: MExpression a -> Bool
---parens _ = True
-parens (MNum _) = False
-parens (MConst _) = False
-parens (MVar _) = False
-parens _ = True
-
-instance Show a => Show (MExpression a) where
-    show (MNum x) = show x
-    show (MConst "pi") = "pi"
-    show (MConst "e") = "e"
-    show (MConst x) = 'C' : T.unpack x
-    show (MVar str) = T.unpack str
-    show (MTerm n x p) = show n <> "*" <> show x <> "^" <> show p
-    show (MOp op xs) = T.unpack op <> show xs
-    --show (MMultiOp op xs) = (getOp op) ++ " " ++ (show $ map (show) xs)
-    --show (MBiOp op x y) | parens x && parens y = "(" ++ show x ++ ") " ++ getOp op ++ " (" ++ show y ++ ")"
-    --                    | parens x = "(" ++ show x ++ ") " ++ getOp op ++ " " ++ show y
-    --                    | parens y = show x ++ " " ++ getOp op ++ " (" ++ show y ++ ")"
-    --                    | otherwise = show x ++ " " ++ getOp op ++ " " ++ show y
-    --show (MUnOp op x) | parens x  = getOp op ++ " (" ++ show x ++ ")"
-    --                  | otherwise = getOp op ++ " " ++ show x
-
---    show (MErr code errval) = "Error " <> show code <> ", with message: " <> T.unpack errval
-
-
-isInfix :: Text -> Bool
-isInfix x = x `elem` ["+", "*", "/", "^"]
-
-percedence :: Text -> Int
-percedence "+" = 1
-percedence "*" = 2
-percedence "/" = 2
-percedence "^" = 3
-percedence _   = -1
 
 ifEq :: Ordering -> Ordering -> Ordering
 ifEq EQ a = a
-ifEq a _  = a
+ifEq a  _ = a
 
-getOrd :: [Ordering] -> Ordering
-getOrd (EQ:xs) = getOrd xs
-getOrd [] = EQ
-getOrd (a:_) = a
+instance (Ord a) => Ord (Math a) where
+    compare (Op x xs) (Op y ys) = compare x y `ifEq` compare xs ys
+    compare Op{} _ = GT
+    compare _ Op{} = LT
+    compare (Sym x) (Sym y) = compare x y
+    compare Sym{} _ = GT
+    compare _ Sym{} = LT
+    compare (Numeric x) (Numeric y) = compare x y
 
-instance Ord a => Ord (MExpression a) where
-    compare (MOp "-" [x]) y = compare x y
-    compare x (MOp "-" [y]) = compare x y
+instance (Ord a, Fractional a) => Num (Math a) where
+    x + y = Op "+" [x, y]
+    x - y = Op "+" [x, Numeric (-1) * y]
+    x * y = Op "*" [x, y]
+    abs x    = Op "abs" [x]
+    negate x = Op "-" [x]
+    signum x = Op "signum" [x]
+    fromInteger x = Numeric (fromInteger x)
 
-    compare (MOp "^" [x1, x2]) (MOp "^" [y1, y2]) = compare x2 y2 `ifEq` compare x1 y1
+instance (Ord a, Fractional a) => Fractional (Math a) where
+    x / y = Op "/" [x, y]
+    fromRational x = Numeric (fromRational x)
 
-    compare (MOp x xs) (MOp y ys) | isInfix x && isInfix y = compare (percedence y) (percedence x)
-                                                             `ifEq` getOrd (zipWith compare xs ys)
-                                  | otherwise = compare x y `ifEq` compare xs ys
+instance (Ord a, Fractional a) => Floating (Math a) where
+    x ** y = Op "^" [x, y]
 
-    compare MOp{} _ = GT
-    compare _ MOp{} = LT
+    pi = Sym "pi"
 
-    compare (MTerm n1 x1 p1) (MTerm n2 x2 p2) = compare p1 p2 `ifEq` (compare x1 x2 `ifEq` compare n1 n2)
-    compare MTerm{} _ = GT
-    compare _ MTerm{} = LT
+    exp x = Sym "e" ** x
+    sqrt x = Op "sqrt" [x]
+    log x = Op "log" [x]
+    logBase x y = Op "logBase" [x, y]
 
-    compare (MConst x) (MConst y) = EQ
-    compare (MConst x) _ = GT
-    compare _ (MConst x) = LT
-    compare (MVar x) (MVar y) = compare y x
-    compare (MVar x) _ = GT
-    compare _ (MVar x) = LT
-    compare (MNum x) (MNum y) = compare x y
+    sin x = Op "sin" [x]
+    cos x = Op "cos" [x]
+    tan x = Op "tan" [x]
 
+    asin x = Op "asin" [x]
+    acos x = Op "acos" [x]
+    atan x = Op "atan" [x]
 
-flatten :: (Ord a) => MExpression a -> MExpression a
-flatten (MOp "+" xs) = MOp "+" . (sortBy $ flip compare) . concat $ flatten' <$> xs
-    where
-        flatten' (MOp "+" xs) = xs
-        flatten' y = [y]
+    sinh x = Op "sinh" [x]
+    cosh x = Op "cosh" [x]
+    tanh x = Op "tanh" [x]
 
-flatten (MOp "*" xs) = MOp "*" . sort . concat $ flatten' <$> xs
-    where
-        flatten' (MOp "*" xs) = xs
-        flatten' y = [y]
-
-
-getTerm :: (Num a) => MExpression a -> MExpression a
-getTerm (MOp "*" [MNum n, MTerm n' x p]) = MTerm (n * n') x p
-getTerm (MOp "*" [MNum n, MOp "^" [x, MNum p]]) = MTerm n x p
-getTerm (MOp "*" [MNum n, x]) = MTerm n x 1
-getTerm (MOp "^" [x, MNum p]) = MTerm 1 x p
-getTerm x = x
-
-traverseM :: (MExpression a -> MExpression a) -> MExpression a -> MExpression a
-traverseM f (MOp x xs) = f . MOp x $ traverseM f <$> xs
-traverseM f e = f e
-
-eqTerm :: (Eq a) => MExpression a -> MExpression a -> Bool
-eqTerm (MTerm _ x p1) (MTerm _ y p2) = x == y && p1 == p2
-eqTerm MNum{} MNum{} = True
-eqTerm (MConst x) (MConst y) = x == y
-eqTerm (MVar x) (MVar y) = x == y
-eqTerm (MOp x xs) (MOp y ys) = x == y && and (zipWith eqTerm xs ys)
-eqTerm _ _ = False
-
-instance (Ord a, Fractional a) => Num (MExpression a) where
-    x + y = calculate . flatten $ MOp "+" [x, y]
-    x - y = calculate . flatten $ MOp "+" [x, negate y]
-    (MOp "/" [a, b]) * c = calculate $ (a*c) / b
-    a * (MOp "/" [b, c]) = calculate $ (a*b) / c
-    x * y = calculate . flatten $ MOp "*" [x, y]
-    abs x    = MOp "abs" [x]
-    negate x = MOp "-" [x]
-    signum x = MOp "signum" [x]
-    fromInteger x = MNum (fromInteger x)
-
-instance (Ord a, Fractional a) => Fractional (MExpression a) where
-    (MOp "/" [x1, y1]) / (MOp "/" [x2, y2]) = calculate $ MOp "/" [x1*x2, y1*y2]
-    (MOp "/" [a, b]) / c = calculate $ MOp "/" [a, b*c]
-    a / (MOp "/" [b, c]) = calculate $ MOp "/" [a*b, c]
-    x / y = MOp "/" [x, y]
-    fromRational x = MNum (fromRational x)
-
-instance (Ord a, Fractional a) => Floating (MExpression a) where
-    pi = MConst "pi"
-    exp x = MOp "^" [MConst "e", x]
-    log x = MOp "log" [x]
-    sqrt x = MOp "sqrt" [x]
-    x ** y = MOp "^" [x, y]
-    logBase x y = MOp "logBase" [x, y]
-    sin x = MOp "sin" [x]
-    cos x = MOp "cos" [x]
-    tan x = MOp "tan" [x]
-    asin x = MOp "asin" [x]
-    acos x = MOp "acos" [x]
-    atan x = MOp "atan" [x]
-    sinh x = MOp "sinh" [x]
-    cosh x = MOp "cosh" [x]
-    tanh x = MOp "tanh" [x]
-    asinh x = MOp "asinh" [x]
-    acosh x = MOp "acosh" [x]
-    atanh x = MOp "atanh" [x]
-
-isNumeric :: (MExpression a) -> Bool
-isNumeric (MNum _) = True
-isNumeric (MOp "-" [MNum _]) = True
-isNumeric _ = False
-
-
-sameType :: MExpression a -> MExpression a -> Bool
-sameType x y = isNumeric x && isNumeric y
-
-
-extractNumeric :: (Num a) => MExpression a -> a
-extractNumeric (MNum a) = a
-extractNumeric (MOp "-" [MNum x]) = -x
-extractNumeric _ = error "Not a numeric"
-
-calculate :: (Fractional a, Ord a) => MExpression a -> MExpression a
-calculate (MOp "+" xs) | all isNumeric $ fmap calculate xs = MNum . sum $ fmap (extractNumeric . calculate) xs
-calculate (MOp "+" xs)  = MOp "+" . concatMap (\x -> if all isNumeric x then [MNum . sum $ fmap extractNumeric x] else x) . groupBy sameType . sort $ fmap calculate xs
-
-calculate (MOp "*" xs) | all isNumeric $ fmap calculate xs = MNum . product $ fmap (extractNumeric . calculate) xs
-calculate (MOp "*" xs)  = MOp "*" . concatMap (\x -> if all isNumeric x then [MNum . product $ fmap extractNumeric x] else x) . groupBy sameType . sort $ fmap calculate xs
-
-calculate (MOp "/" xs) | all isNumeric $ fmap calculate xs = MNum . foldr (/) 1 $ fmap (extractNumeric . calculate) xs
-calculate (MOp "/" xs)  = MOp "/" . concatMap (\x -> if all isNumeric x then [MNum . foldr (/) 1 $ fmap extractNumeric x] else x) . groupBy sameType . sort $ fmap calculate xs
-
-calculate (MOp op xs) = MOp op $ fmap calculate xs
-calculate x = x
+    asinh x = Op "asinh" [x]
+    acosh x = Op "acosh" [x]
+    atanh x = Op "atanh" [x]
 
 
 
-a = MVar "a"
-b = MVar "b"
-c = MVar "c"
-d = MVar "d"
-e = MVar "e"
-f = MVar "f"
+a = Sym "a"
+b = Sym "b"
+c = Sym "c"
+d = Sym "d"
+e = Sym "e"
+f = Sym "f"
--- src/Math/Symbolic/Simplify.hs
+++ src/Math/Symbolic/Simplify.hs
@@ -12,52 +12,197 @@ import Control.Arrow
 import qualified Data.Text as T
 import Data.Text (Text)
 
-flatten :: MExpression a -> MExpression a
-flatten (MOp "+" xs) = MOp "+" . concat $ flatten' <$> xs
+
+isOp :: Text -> Math a -> Bool
+isOp x (Op y _) = x == y
+isOp _ _ = False
+
+takeOp :: Text -> [Math a] -> ([Math a], Math a)
+takeOp op ms = takeOp' ms []
     where
-        flatten' (MOp "+" xs) = xs
-        flatten' y = [y]
+        takeOp' (x:xs) ys | isOp op x = (xs <> ys, x)
+                          | otherwise = takeOp' xs $ x:ys
 
-flatten (MOp "*" xs) = MOp "*" . concat $ flatten' <$> xs
+
+
+converge :: (a -> a -> Bool) -> [a] -> a
+converge p (x:ys@(y:_))
+    | p x y     = y
+    | otherwise = converge p ys
+
+
+runFun :: (Eq a) => (a -> a) -> a -> a
+runFun f a = converge (==) $ iterate f a
+
+percedende :: Text -> Int
+percedende "+" = 1
+percedende "-" = 1
+percedende "*" = 2
+percedende "/" = 2
+percedende "^" = 3
+percedende _ = 3
+
+opCompare :: Text -> Text -> Ordering
+opCompare x y | percedende x == percedende y = EQ
+              | percedende x > percedende y  = GT
+              | otherwise                    = LT
+
+
+mathCompare :: (Ord a) => Math a -> Math a -> Ordering
+mathCompare (Op "^" [x, xs]) (Op "^" [y, ys]) = mathCompare (mathSort xs) (mathSort ys) `ifEq` mathCompare x y
+mathCompare (Op "*" [Numeric x, xs])  (Op "*" [Numeric y, ys]) = mathCompare (mathSort xs) (mathSort ys) `ifEq` compare y x
+mathCompare (Op "*" [Numeric _, x]) y = mathCompare x y
+mathCompare x (Op "*" [Numeric _, y]) = mathCompare x y
+mathCompare (Op x xs) (Op y ys) = opCompare y x `ifEq` foldr ifEq EQ (zipWith mathCompare xs ys)
+mathCompare Op{} _ = LT
+mathCompare _ Op{} = GT
+mathCompare (Sym x) (Sym y) = compare x y
+mathCompare Sym{} _ = LT
+mathCompare _ Sym{} = GT
+mathCompare (Numeric x) (Numeric y) = compare y x
+
+
+multiCompare :: (Ord a) => Math a -> Math a -> Ordering
+multiCompare (Numeric x) (Numeric y) = compare y x
+multiCompare (Numeric _) _ = LT
+multiCompare _ (Numeric _) = GT
+multiCompare x y = mathCompare x y
+
+mathSort :: (Ord a) => Math a -> Math a
+mathSort (Op "+" xs) = Op "+" . sortBy mathCompare $ mathSort <$> xs
+mathSort (Op "*" xs) = Op "*" . sortBy multiCompare $ mathSort <$> xs
+mathSort (Op op xs)  = Op op $ mathSort <$> xs
+mathSort x = x
+
+
+level :: (Ord a) => Math a -> Math a
+level (Op "+" xs) = Op "+" . fmap level . concat $ level' <$> xs
     where
-        flatten' (MOp "*" xs) = xs
-        flatten' y = [y]
+        level' (Op "+" xs) = xs
+        level' y = [y]
+
+level (Op "*" xs) = Op "*" . fmap level . concat $ level' <$> xs
+    where
+        level' (Op "*" xs) = xs
+        level' y = [y]
 
 
-{-
-toAddTerm :: (Fractional a, Ord a) => MExpression a -> MExpression a
-toAddTerm (MOp "*" [n@(MNum _), op@(MOp _ _)]) = n * toAddTerm op
-toAddTerm t@(MOp "*" [MNum n, x]) = t
-toAddTerm x@(MVar _) = 1 * x
-toAddTerm (MOp op xs) = MOp op $ fmap toAddTerm xs
-toAddTerm x = x
--}
+level (Op op xs) = Op op $ level <$> xs
 
-toMTerm :: (Fractional a, Ord a) => MExpression a -> MExpression a
-toMTerm t@(MOp "^" _) = t
-toMTerm x@(MVar _)    = x**1
-toMTerm (MOp op xs)   = MOp op $ fmap toMTerm xs
+level x = x
+
+simplifyRational :: (Ord a, Fractional a) => Math a -> Math a
+simplifyRational (Op "/" [Op "/" [a, b], c]) = a / (b*c)
+simplifyRational (Op "/" [a, Op "/" [b, c]]) = (a*b) / c
+simplifyRational (Op "*" xs) | any (isOp "/") xs = let (as, Op "/" [b, c]) = takeOp "/" xs
+                                                   in Op "*" (b:as) / c
+simplifyRational (Op op xs) = Op op $ simplifyRational <$> xs
+simplifyRational x = x
+
+
+
+toMTerm :: (Ord a, Fractional a) => Math a -> Math a
+toMTerm (Op "*" xs) = Op "*" $ toMTerm' . toMTerm <$> xs
+    where
+        toMTerm' x@(Op "^" _)  = x
+    --    toMTerm' x@(Numeric _) = x
+        toMTerm' x = x**1
+toMTerm (Op op xs)  = Op op $ toMTerm <$> xs
 toMTerm x = x
 
-likeMTerm :: (Fractional a, Ord a) => MExpression a -> MExpression a -> Bool
-likeMTerm x y | x == y = True
-likeMTerm (MOp "^" [x1, y1]) (MOp "^" [x2, y2]) = x1 == x2
+
+toSTerm :: (Ord a, Fractional a) => Math a -> Math a
+toSTerm (Op "+" xs) = Op "+" $ toSTerm' . toSTerm <$> xs
+    where
+
+        toSTerm' x@(Op "*" (Numeric _:_))  = x
+        toSTerm' (Op "*" xs)  = Op "*" $ 1:xs
+    --    toSTerm' x@(Numeric _) = x
+        toSTerm' x = 1*x
+toSTerm (Op op xs)  = Op op $ toSTerm <$> xs
+toSTerm x = x
+
+
+likeMTerm :: (Ord a, Fractional a) => Math a -> Math a -> Bool
+likeMTerm (Op "^" [x, xs]) (Op "^" [y, ys]) = x == y
 likeMTerm _ _ = False
 
-getExponent :: MExpression a -> MExpression a
-getExponent (MOp "^" [x, y]) = y
-getExponent _ = error "Not a power"
 
-getBase :: MExpression a -> MExpression a
-getBase (MOp "^" [x, y]) = x
-getBase _ = error "Not a power"
+likeSTerm :: (Ord a, Fractional a) => Math a -> Math a -> Bool
+likeSTerm (Op "*" (Numeric _ :xs)) (Op "*" (Numeric _:ys)) = xs == ys
+likeSTerm _ _ = False
 
-getLikeM :: (Fractional a, Ord a) => MExpression a -> MExpression a
-getLikeM (MOp "*" xs) = MOp "+" . fmap (\x -> getBase (head x) ** sum (fmap getExponent x)) . groupBy likeMTerm . sort $ fmap getLikeM xs
-getLikeM (MOp op xs) = MOp op $ fmap getLikeM xs
-getLikeM x = x
 
-getLike :: (Fractional a, Ord a) => MExpression a -> MExpression a
-getLike (MOp "+" xs) = MOp "+" . fmap (\x -> head x * fromIntegral (length x)) . group . sort $ fmap getLike xs
-getLike (MOp op xs) = MOp op $ fmap getLike xs
-getLike x = x
+collectMLike :: (Ord a, Fractional a) => Math a -> Math a
+collectMLike (Op "*" xs) = Op "*" . concatMap addlike . groupBy likeMTerm $ collectMLike <$> xs
+    where
+        addlike (Op "^" [x, y] : xs) = [x ** foldr1 (+) (y:fmap getExponent xs)]
+        addlike x = x
+        getExponent (Op "^" [_, y]) = y
+
+collectMLike (Op op xs) = Op op $ collectMLike <$> xs
+collectMLike x = x
+
+
+collectSLike :: (Ord a, Fractional a) => Math a -> Math a
+collectSLike (Op "+" xs) = Op "+" . concatMap addlike . groupBy likeSTerm $ collectSLike <$> xs
+    where
+        addlike (Op "*" (x@(Numeric _):xs) : ys) = [foldr1 (+) (x:fmap getNumeric ys) * (foldr1 (*) xs)]
+        addlike x = x
+        getNumeric (Op "*" (x@(Numeric _):_)) = x
+
+collectSLike (Op op xs) = Op op $ collectSLike <$> xs
+collectSLike x = x
+
+
+reduce :: (Ord a, Fractional a) => Math a -> Math a
+reduce (Op "+" [x]) = x
+reduce (Op "*" [x]) = x
+reduce (Op "^" [x, 1]) = reduce x
+reduce (Op "*" (1:xs)) = reduce . Op "*" $ reduce <$> xs
+reduce (Op "*" (0:_))  = Numeric 0
+reduce (Op op xs) = foldConst . Op op $ reduce <$> xs
+reduce x = x
+
+
+foldConst :: (Ord a, Fractional a) => Math a -> Math a
+foldConst (Op "+" xs) = Op "+" . concatMap (\x -> if all isNumeric xs then [Numeric . sum $ getNumeric <$> xs] else x) . groupBy sameType $ foldConst <$> xs
+foldConst (Op "*" xs) = Op "*" . concatMap (\x -> if all isNumeric xs then [Numeric . product $ getNumeric <$> xs] else x) . groupBy sameType $ foldConst <$> xs
+foldConst (Op op xs) = Op op $ foldConst <$> xs
+foldConst x = x
+
+
+isNumeric Numeric{} = True
+isNumeric (Op "-" [Numeric{}]) = True
+isNumeric _ = False
+getNumeric (Numeric x) = x
+--getNumeric (Op "-" [Numeric x]) = -x
+getNumeric _ = error "not a numeric"
+
+sameType :: Math a -> Math a -> Bool
+sameType Numeric{} Numeric{} = True
+--sameType (Op "-" [x]) y = sameType x y
+--sameType ()
+sameType Sym{} Sym{}         = True
+sameType (Op x _) (Op y _)   = x == y
+sameType _ _ = False
+
+
+likeTerms :: (Ord a, Fractional a) => Math a -> Math a
+likeTerms = runFun reduce
+          . collectSLike
+          . toSTerm
+          . runFun reduce
+          . collectMLike
+          . toMTerm
+
+
+simplify :: (Ord a, Fractional a) => Math a -> Math a
+simplify = runFun simplify' . mathSort
+    where
+        simplify' = mathSort
+                  . likeTerms
+                  . runFun reduce
+                  . runFun simplifyRational
+                  . runFun level
+                 -- . toSTerm
